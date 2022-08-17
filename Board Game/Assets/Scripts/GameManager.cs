a2bbe949437fe45f56f6505c32cc19c7262fbe9e
Kerim <MCKerim@gmx.de>
2022-08-17 23:52:54 +0200

    Added Prototype Draw Card Button
    - Added Prototype Draw Card button
    - Added Player Colors for current player text

--- Board Game/Assets/Scripts/GameManager.cs
+++ Board Game/Assets/Scripts/GameManager.cs
@@ -8,7 +8,11 @@ public class GameManager : MonoBehaviour
     private Board board;
     private int currentPlayer;
 
+    [SerializeField] private Color[] playerColors;
+
     [SerializeField] private TextMeshProUGUI currentPlayerText;
+
+    private bool listenForFieldPresses = true;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +20,7 @@ public class GameManager : MonoBehaviour
         board = GameObject.FindObjectOfType<Board>();
 
         currentPlayer = 1;
-        currentPlayerText.SetText("Player " + currentPlayer);
+        UpdateCurrentPlayerText();
     }
 
     // Update is called once per frame
@@ -25,10 +29,19 @@ public class GameManager : MonoBehaviour
         
     }
 
-    int buildingsPlaced = 0;
+    public void DrawCardButonPressed()
+    {
+        NextPlayer();
+    }
 
+    int buildingsPlaced = 0;
     public void FieldPressed(int x, int y)
     {
+        if (!listenForFieldPresses)
+        {
+            return;
+        }
+
         if (board.CheckIfFieldIsFree(x, y))
         {
             if(board.CheckIfFieldIsValidForPlayerToBuild(currentPlayer, x, y) || buildingsPlaced < 2)
@@ -58,16 +71,19 @@ public class GameManager : MonoBehaviour
 
     private void NextPlayer()
     {
-        if(currentPlayer == 1)
-        {
-            currentPlayer = 2;
-        }
-        else if(currentPlayer == 2)
+        currentPlayer++;
+        if(currentPlayer-1 >= playerColors.Length)
         {
             currentPlayer = 1;
         }
 
+        UpdateCurrentPlayerText();
+    }
+
+    private void UpdateCurrentPlayerText()
+    {
         currentPlayerText.SetText("Player " + currentPlayer);
+        currentPlayerText.color = playerColors[currentPlayer - 1];
     }
 
 }
